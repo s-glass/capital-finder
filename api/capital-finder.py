18e5d1bfee3f85a3fa644ba78e91db7be09c459e
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-07-08 17:02:27 -0700

    edits

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -11,10 +11,7 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        # response code
         self.send_response(200)
-
-        # headers
         self.headers.add_header("Content-type", "text/plain")
         self.end_headers()
 
@@ -32,11 +29,11 @@ class handler(BaseHTTPRequestHandler):
             return
         
         elif capital:
-            url ='https://restcountries.com/v3.1/capital'
+            url ="https://restcountries.com/v3.1/capital/"
             capital_response = requests.get(url + capital)
             capital_data = capital_response.json()
             country_message = capital_data[0]['name']['common']
             output_country = str(f'{capital} is the capital of {country_message}')
-            display_country_message = str{output_country}
+            display_country_message = str(output_country)
             self.wfile.write(display_country_message.encode())
             return
